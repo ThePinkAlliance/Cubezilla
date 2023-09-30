cf9847728cf8224988ab78723a7d4e9642243138
TopHatTacco <105398626+TopHatTacco@users.noreply.github.com>
2023-09-30 11:04:26 -0600

    added arm and shooter subystems (files)

--- src/main/java/frc/robot/commands/JoystickDrive.java
+++ src/main/java/frc/robot/commands/JoystickDrive.java
@@ -37,6 +37,7 @@ public class JoystickDrive extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
